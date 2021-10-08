d28ad31aa3fcb696834fb5210084c75e9ff02c3d
Kate Kasuba <otroshko12@gmail.com>
2021-10-08 12:13:25 +0300

    Updates

---
+++ precache-manifest.b65190bf8ca991a9703f09efa9b3cbc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383163ac8444f4ecdb4912e6b00c2add",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d05f3490db28a398736",
+    "url": "./static/css/main.68fff2fd.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d05f3490db28a398736",
+    "url": "./static/js/main.a7d94fa1.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
